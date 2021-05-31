7839f660e3f655cc1b5d43f37aabc35f142fff22
Bibin Sebastian <bibinvamattathil@gmail.com>
2021-05-31 17:06:21 +0530

    bug fixes: syntax errors

--- app.py
+++ app.py
@@ -6,10 +6,36 @@ from flask import render_template, request, send_from_directory, flash, url_for
 from flask import current_app as app
 from werkzeug.utils import secure_filename
 
+from src.lstm import ActionClassificationLSTM
+from src.video_analyzer import analyse_video
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+
 app = Flask(__name__)
 UPLOAD_FOLDER = './'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "secret key" 
+
+
+# detectron config
+start = time.time()
+cfg = get_cfg()
+# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")
+pose_detector = DefaultPredictor(cfg)
+model_load_done = time.time()
+print("model_load", model_load_done - start)
+
+#lstm
+lstm_classifier = ActionClassificationLSTM.load_from_checkpoint("models/saved_model.ckpt")
+lstm_classifier.eval()
+
 
 class DataObject():
     pass
@@ -58,6 +84,12 @@ def upload():
 @app.route('/files/<filename>')
 def get_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/analyze/<filename>')
+def analyze(filename):
+    stream = analyse_video(pose_detector, lstm_classifier, filename)
+    return Response(stream,
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
